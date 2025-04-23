308772336c24d71bbd85494e3460348fc7d023d2
Dynamicssquareca <dynamicssquareca@gmail.com>
2025-04-23 20:06:13 +0530

    update code

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
                 <p>We specialize in creating premium quality memorial headstones and gravestones using the finest granite</p>
                 <div className='hero-banner-btn'>
                 <a className='btn btn-three'>Get Quotes</a>
-                <a className='btn bbtn-transparent'>Request Catalogue</a>
+                <a className='btn btn-transparent'>Request Catalogue</a>
                 </div>
                 
               </div>
@@ -30,6 +30,72 @@ export default function Home() {
         </div>
 
       </div>
+      <section className='about-us'>
+        <div className='container'>
+          <div className='row align-items-center justify-content-center'>
+            <div className='col-lg-6'>
+              <div className='about-us-content'>
+                <h2>About Us</h2>
+                <p>We are a family-run business with over 20 years of experience in the stone industry. We pride ourselves on our craftsmanship and attention to detail.</p>
+                <a className='btn btn-three'>Learn More</a>
+              </div>
+            </div>
+            <div className='col-lg-6'>
+              <img src='/img/about-us.jpg' alt='About Us' />
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='about-us'>
+        <div className='container'>
+          <div className='row align-items-center justify-content-center'>
+            <div className='col-lg-6'>
+              <div className='about-us-content'>
+                <h2>About Us</h2>
+                <p>We are a family-run business with over 20 years of experience in the stone industry. We pride ourselves on our craftsmanship and attention to detail.</p>
+                <a className='btn btn-three'>Learn More</a>
+              </div>
+            </div>
+            <div className='col-lg-6'>
+              <img src='/img/about-us.jpg' alt='About Us' />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='about-us'>
+        <div className='container'>
+          <div className='row align-items-center justify-content-center'>
+            <div className='col-lg-6'>
+              <div className='about-us-content'>
+                <h2>About Us</h2>
+                <p>We are a family-run business with over 20 years of experience in the stone industry. We pride ourselves on our craftsmanship and attention to detail.</p>
+                <a className='btn btn-three'>Learn More</a>
+              </div>
+            </div>
+            <div className='col-lg-6'>
+              <img src='/img/about-us.jpg' alt='About Us' />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='about-us'>
+        <div className='container'>
+          <div className='row align-items-center justify-content-center'>
+            <div className='col-lg-6'>
+              <div className='about-us-content'>
+                <h2>About Us</h2>
+                <p>We are a family-run business with over 20 years of experience in the stone industry. We pride ourselves on our craftsmanship and attention to detail.</p>
+                <a className='btn btn-three'>Learn More</a>
+              </div>
+            </div>
+            <div className='col-lg-6'>
+              <img src='/img/about-us.jpg' alt='About Us' />
+            </div>
+          </div>
+        </div>
+      </section>
 
 
 
