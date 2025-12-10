fa324ccaddedd9ae5ca18e7c204a1f47568f0bf8
Dynamicssquareca <dynamicssquareca@gmail.com>
2025-12-10 13:01:02 +0530

    update code

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default function Document() {
         <link rel="icon" type="image/png" href="/favicon.png" sizes="192x192" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
 
       </Head>
