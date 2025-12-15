5bb51a5b4d9d3c0421145756a0c58a8b30b74cff
Dynamicssquareca <dynamicssquareca@gmail.com>
2025-12-15 16:49:41 +0530

    update new code

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.stonediscover.com/",
-  generateRobotsTxt: false,
+  siteUrl: "https://www.stonediscover.com",
+  generateRobotsTxt: true, // ✅ Recommended to enable, unless you create robots.txt manually
   generateIndexSitemap: false,
   sitemapSize: 50000,
 
@@ -10,12 +10,34 @@ module.exports = {
     "/blog/tag*",
     "/blog/category*",
     "/blog/author*",
+    "/natural-stones",
+    "/natural-stones/*",
   ],
 
   additionalPaths: async (config) => {
-    // Add as many manual URLs as you want
+    // ✅ Add manual URLs here
     const manualUrls = [
-    //   "/page-1"
+      "/tombstones-monuments/angel/",
+      "/tombstones-monuments/bespoke/",
+      "/tombstones-monuments/bevel-markers/",
+      "/tombstones-monuments/book/",
+      "/tombstones-monuments/columbarium/",
+      "/tombstones-monuments/flat/",
+      "/tombstones-monuments/vases/",
+      "/tombstones-monuments/headstones/",
+      "/tombstones-monuments/kerb-sets/",
+      "/tombstones-monuments/bench/",
+      "/tombstones-monuments/plaques/",
+      "/tombstones-monuments/urns/",
+      "/tombstones-monuments/pet/",
+      "/tombstones-monuments/slant/",
+      "/tombstones-monuments/upright/",
+      "/location/headstones-in-atlanta-georgia/",
+      "/location/brooklyn/",
+      "/location/detroit/",
+      "/location/los-angeles/",
+      "/location/lubbock/",
+      "/location/orlando/",
     ];
 
     return manualUrls.map((url) => config.transform(config, url));
