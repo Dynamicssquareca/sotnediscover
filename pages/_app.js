edf9a97877761e992bdf2744c4865f1a3070a9c9
Dynamicssquareca <dynamicssquareca@gmail.com>
2025-12-17 12:33:14 +0530

    update app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,33 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-icons/font/bootstrap-icons.min.css"
+import { useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
 import "@/styles/globals.css";
-import "@/styles/header.css"
-import "@/styles/footer.css"
-import Layout from '@/components/Layout'; // Import your custom layout component
+import "@/styles/header.css";
+import "@/styles/footer.css";
+
+import Layout from "@/components/Layout";
 import Head from "next/head";
 
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />;
-// }
-
-
 function MyAppWithLayout({ Component, pageProps }) {
- 
 
- {
-    return (
-      <>
+  // 🔹 Remove Google srsltid parameter (optional cleanup)
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const url = new URL(window.location.href);
+      if (url.searchParams.has("srsltid")) {
+        url.searchParams.delete("srsltid");
+        window.history.replaceState({}, "", url.pathname + url.search);
+      }
+    }
+  }, []);
+
+  return (
+    <>
       <Layout faq={pageProps.faq}>
         <Component {...pageProps} />
       </Layout>
-      </>
-    );
-  }
-
-
+    </>
+  );
 }
 
 export default MyAppWithLayout;
